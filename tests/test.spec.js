c2c3cc2993f1d41234801d7285e46f585ce81356
Cristian Hoyos Carvajal <dianacaro46@gmail.com>
2025-11-04 10:56:29 -0500

    test fixed

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,28 +1,38 @@
-const { chromium } = require('playwright');
-const browser = await firefox.launch();
-const { test, expect } = require('@playwright/test');
+import { test, expect, firefox } from "@playwright/test";
 
-test.describe('My Test Suite', () => {
-   test.setTimeout(60000);
-  test('My Test Case', async ({}) => {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
-  
-  await authenticate(page);
+test.describe("My Test Suite", () => {
+  test.setTimeout(60000);
 
-  await browser.close();
+  test("My Test Case", async () => {
+    const browser = await firefox.launch();
+    const page = await browser.newPage();
 
+    try {
+      await authenticate(page);
+    } finally {
+      await browser.close();
+    }
   });
 });
 
 async function authenticate(page) {
-  await page.goto('https://bitheap.tech');
-  await page.click('#menu-item-1311'); 
-  await page.locator("[name='xoo-el-username']").fill(process.env.BITHEAP_USERNAME)
-  await page.locator("[name='xoo-el-password']").fill(process.env.PASS)
-  await page.locator('xpath=/html/body/div[2]/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/form/button').click()
-  const text = await page.locator('css=#menu-item-1314 > a').textContent()
-  if(text != "Hello, Playwright") 
-    console.error("The authentication was not successful!")
-  await page.screenshot({ path: 'screenshot.png' });
+  await page.goto("https://bitheap.tech");
+  await page.click("#menu-item-2330");
+  await page.locator("[name='xoo-el-username']").fill("playwright");
+  await page.locator("[name='xoo-el-password']").fill("playwright");
+  await page
+    .locator(
+      "css=body > div.xoo-el-container.xoo-el-style-popup.xoo-el-popup-active > div.xoo-el-modal > div > div > div.xoo-el-srcont > div > div > div.xoo-el-section.xoo-el-active > div > form > button"
+    )
+    .click();
+  const text = await page
+    .locator(
+      "css=#page > div.site.kv-site.kv-main > content > div > section.background-id_808080m16.section-2.meruhy39.undefined.kv-full-page > div.position-relative.kv-content > div > div > div > div:nth-child(1) > h2"
+    )
+    .textContent();
+
+  if (text !== "Welcome to bitheap") {
+    console.error("The authentication was not successful!");
+  }
+  await page.screenshot({ path: "screenshot.png" });
 }
